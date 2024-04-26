82563cc76b4894ce6a4ab650ed7a7463a8cc4795
viveks-indira <viveks@indiratrade.com>
2024-04-26 10:39:03 +0530

    Final

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,73 @@
-import {
-  BrowserRouter as Router,
-} from "react-router-dom";
-import {
-  Arwes,
-  SoundsProvider,
-  ThemeProvider,
-  createSounds,
-  createTheme,
-} from "arwes";
+// import React from "react";
+// import { BrowserRouter as Router } from "react-router-dom";
+// import { Arwes } from "arwes";
 
-import AppLayout from "./pages/AppLayout";
+// import AppLayout from "./pages/AppLayout";
+// import { resources } from "./settings";
 
-import { theme, resources, sounds } from "./settings";
+// // Define a basic theme object
+// const theme = {
+//   color: {
+//     primary: "#00FF00", // Example primary color
+//     // Define other theme colors here as needed
+//   },
+//   // Other theme properties can be added here
+// };
 
+// const App = () => {
+//   return (
+//     <Arwes animate background={resources.background.large} pattern={resources.pattern} theme={theme}>
+//       {(anim) => (
+//         <Router>
+//           <AppLayout show={anim.entered} />
+//         </Router>
+//       )}
+//     </Arwes>
+//   );
+// };
+
+// export default App;
+
+import React from "react";
+import styled from "styled-components";
+
+// Define custom styled components
+const CustomContainer = styled.div`
+  /* Add your custom container styles here */
+`;
+
+const CustomHeader = styled.header`
+  /* Add your custom header styles here */
+`;
+
+const CustomContent = styled.div`
+  /* Add your custom content styles here */
+`;
+
+const CustomFooter = styled.footer`
+  /* Add your custom footer styles here */
+`;
+
+// Define the custom component
+const CustomArwes = ({ children, background, pattern }) => {
+  return (
+    <CustomContainer style={{ background, pattern }}>
+      <CustomHeader>{children[0]}</CustomHeader>
+      <CustomContent>{children[1]}</CustomContent>
+      <CustomFooter>{children[2]}</CustomFooter>
+    </CustomContainer>
+  );
+};
+
+// Usage example:
 const App = () => {
-  return <ThemeProvider theme={createTheme(theme)}>
-    <SoundsProvider sounds={createSounds(sounds)}>
-      <Arwes animate background={resources.background.large} pattern={resources.pattern}>
-        {anim => (
-          <Router>
-            <AppLayout show={anim.entered} />
-          </Router>
-        )}
-      </Arwes>
-    </SoundsProvider>
-  </ThemeProvider>;
+  return (
+    <CustomArwes background="#000" pattern="squiggles">
+      <header>This is the header</header>
+      <div>This is the content</div>
+      <footer>This is the footer</footer>
+    </CustomArwes>
+  );
 };
 
 export default App;
--- server/src/app.js
+++ server/src/app.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "..", "public")));
 app.use('/v1',api);
 
-app.get("/*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
 
